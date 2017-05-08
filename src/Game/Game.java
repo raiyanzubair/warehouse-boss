ae4d39719662a331273d38af2e036de861a1804c
shawnmanuel000 <shawnmanuel000@gmail.com>
2017-05-08 13:44:52 +1000

    Update Game.java

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -65,19 +65,13 @@ public class Game
 	private class KeyAction implements KeyListener
 	{
 		@Override
-		public void keyTyped(KeyEvent e)
-		{
-		}
+		public void keyTyped(KeyEvent e){}
 
 		@Override
-		public void keyPressed(KeyEvent e)
-		{
-		}
+		public void keyPressed(KeyEvent e){}
 
 		@Override
-		public void keyReleased(KeyEvent e)
-		{
-		}	
+		public void keyReleased(KeyEvent e){}	
 	}
 	
 }
