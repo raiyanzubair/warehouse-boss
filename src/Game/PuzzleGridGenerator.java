d99ca7339fce4cff1e020896d0f65bd498d95327
shawnmanuel000 <shawnmanuel000@gmail.com>
2017-05-20 20:51:49 +1000

    Update PuzzleGridGenerator.java

--- src/Game/PuzzleGridGenerator.java
+++ src/Game/PuzzleGridGenerator.java
@@ -1,14 +1,8 @@
 package Game;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
+import java.util.ArrayList;
+
+import Game.ImageFactory.Type;
 
 /**
  * This class manages the generation of puzzles defined as an array of 'Type' enums
@@ -21,107 +15,19 @@ import javax.swing.ImageIcon;
  */
 public class PuzzleGridGenerator
 {
-	private static final int DESIRED_HEIGHT = 50;
-	public static enum Type
-	{
-		EMPTY,
-		BRICK,
-		BOX,
-		GREENBOX,
-		CROSS,
-		MANDOWN,
-		MANUP,
-		MANLEFT,
-		MANRIGHT
-	}
-	private static String[] images =
-	{
-		"Empty.png",
-		"Brick.png",
-		"Box.png",
-		"GreenBox.png",
-		"Cross.png",
-		"ManDown.png",
-		"ManUp.png",
-		"ManLeft.png",
-		"ManRight.png"
-	};
-	public static ImageIcon[] icons = new ImageIcon[images.length];
 	private ArrayList<PuzzleGrid> levels;
 
 	public PuzzleGridGenerator()
 	{
-		loadImages();
 		this.levels = new ArrayList<PuzzleGrid>();
 		this.populateLevels();
 	}
 	
-	/**
-	 * Initializes the images array with the ImageIcons from the filenames
-	 */
- 	private static void loadImages()
+	public int getNumberOfLevels()
 	{
-		for(Type t : Type.values())
-		{
-			int index = t.ordinal();
-			icons[index] =loadIcon(images[index]);
-		}
+		return levels.size();
 	}
-
- 	/**
- 	 * Converts an image file loaded from a filename into an ImageIcon
- 	 * @param filename: The name of the file to convert
- 	 * @return: The converted ImageIcon
- 	 */
-	private static ImageIcon loadIcon(String filename)
-	{
-		return new ImageIcon(resizeImage(filename));
-	}
-
-	/**
-	 * Loads an image of the given filename and resizes its dimensions
-	 * to have the same height as DESIRED_HEIGHT
-	 * @param filename: The name of the image file to load and resize
-	 * @return: The resized image
-	 */
-	private static BufferedImage resizeImage(String filename)
-	{
-		BufferedImage originalImage = loadImage(filename);
-
-		int originalHeight = originalImage.getHeight();
-		int originalWidth = originalImage.getWidth();
-		int newHeight = DESIRED_HEIGHT;
-		int newWidth = newHeight * (originalWidth/originalHeight);
-
-		BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
-		Graphics2D g = resizedImage.createGraphics();
-
-		g.drawImage(originalImage, 0, 0, newWidth, newHeight, null);
-		g.dispose();
-
-		return resizedImage;
-	}
-
-	/**
-	 * Looks for the image with the given filename in the Icons74 directory
-	 * and returns the image as a BufferedImage
-	 * @param filename: The name of the file to load
-	 * @return: The loaded BufferedImage
-	 */
-	private static BufferedImage loadImage(String filename)
-	{
-		BufferedImage bimg = null;
-		try
-		{
-			bimg = ImageIO.read(new File("src/icons74/"+filename));
-		}
-		catch (IOException e)
-		{
-			Logger.getLogger(PuzzleLabel.class.getName()).log(Level.SEVERE, null, e);
-		}
-		return bimg;
-	}
-
+	
 	/**
 	 * Creates a puzzle grid object with an array of image types to place in the
 	 * puzzle and the dimensions to arrange the images in a grid of the form
@@ -200,5 +106,4 @@ public class PuzzleGridGenerator
 			Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 
 		}));
 	}
-
 }
