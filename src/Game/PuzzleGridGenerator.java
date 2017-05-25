a1c7196a0b5cd5e6e62ffa4934775f54f44c5687
Jaiden Choy <z5062569@student.unsw.edu.au>
2017-05-26 01:00:51 +1000

    Added P2 to the new map

--- src/Game/PuzzleGridGenerator.java
+++ src/Game/PuzzleGridGenerator.java
@@ -210,18 +210,18 @@ public class PuzzleGridGenerator
 		}));
 		
 		multiPlayerLevels.add(numLevels++, new PuzzleGrid(true, 1, 10, 12, new Type[]
-				{
-					Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK,		Type.BRICK,  	Type.BRICK,	 	Type.BRICK,
-					Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.EMPTY, 	Type.EMPTY, 	Type.EMPTY,	 	Type.BRICK,  	Type.BRICK,		Type.BRICK,
-					Type.BRICK, 	Type.EMPTY, 	Type.EMPTY, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.EMPTY, 	Type.BRICK, 	Type.EMPTY, 	Type.EMPTY, 	Type.BRICK, 	Type.BRICK,
-					Type.BRICK, 	Type.EMPTY, 	Type.P1_BOX, 	Type.EMPTY, 	Type.P1_CROSS, 	Type.P1_CROSS, 	Type.P1_CROSS, 	Type.P1_CROSS, 	Type.BRICK, 	Type.EMPTY, 	Type.BRICK,		Type.BRICK,
-					Type.BRICK, 	Type.P1_RIGHT, 	Type.EMPTY, 	Type.BRICK, 	Type.BRICK, 	Type.P2_CROSS, 	Type.P2_CROSS, 	Type.P2_BOXED, 	Type.P2_CROSS, 	Type.EMPTY, 	Type.EMPTY,		Type.BRICK,
-					Type.BRICK, 	Type.EMPTY, 	Type.P2_BOX,	Type.EMPTY, 	Type.EMPTY, 	Type.P2_BOX, 	Type.BRICK, 	Type.BRICK, 	Type.P1_BOX, 	Type.BRICK, 	Type.EMPTY,		Type.BRICK,
-					Type.BRICK, 	Type.EMPTY, 	Type.EMPTY, 	Type.BRICK, 	Type.P1_BOX, 	Type.EMPTY, 	Type.EMPTY, 	Type.EMPTY, 	Type.P2_BOX, 	Type.EMPTY,	 	Type.EMPTY,		Type.BRICK,
-					Type.BRICK, 	Type.BRICK, 	Type.EMPTY, 	Type.EMPTY, 	Type.EMPTY, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.P1_BOX, 	Type.BRICK,	 	Type.EMPTY,		Type.BRICK,	
-					Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.EMPTY, 	Type.EMPTY,	 	Type.EMPTY,		Type.BRICK,
-					Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK,	 	Type.BRICK,		Type.BRICK,
-				}));
+		{
+			Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK,		Type.BRICK,  	Type.BRICK,	 	Type.BRICK,
+			Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.EMPTY, 	Type.EMPTY, 	Type.EMPTY,	 	Type.BRICK,  	Type.BRICK,		Type.BRICK,
+			Type.BRICK, 	Type.EMPTY, 	Type.EMPTY, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.EMPTY, 	Type.BRICK, 	Type.EMPTY, 	Type.EMPTY, 	Type.BRICK, 	Type.BRICK,
+			Type.BRICK, 	Type.EMPTY, 	Type.P1_BOX, 	Type.EMPTY, 	Type.P1_CROSS, 	Type.P1_CROSS, 	Type.P1_CROSS, 	Type.P1_CROSS, 	Type.BRICK, 	Type.EMPTY, 	Type.BRICK,		Type.BRICK,
+			Type.BRICK, 	Type.P1_RIGHT, 	Type.EMPTY, 	Type.BRICK, 	Type.BRICK, 	Type.P2_CROSS, 	Type.P2_CROSS, 	Type.P2_BOXED, 	Type.P2_CROSS, 	Type.EMPTY, 	Type.EMPTY,		Type.BRICK,
+			Type.BRICK, 	Type.EMPTY, 	Type.P2_BOX,	Type.EMPTY, 	Type.EMPTY, 	Type.P2_BOX, 	Type.BRICK, 	Type.BRICK, 	Type.P1_BOX, 	Type.BRICK, 	Type.EMPTY,		Type.BRICK,
+			Type.BRICK, 	Type.EMPTY, 	Type.EMPTY, 	Type.BRICK, 	Type.P1_BOX, 	Type.EMPTY, 	Type.EMPTY, 	Type.EMPTY, 	Type.P2_BOX, 	Type.EMPTY,	 	Type.P2_RIGHT,	Type.BRICK,
+			Type.BRICK, 	Type.BRICK, 	Type.EMPTY, 	Type.EMPTY, 	Type.EMPTY, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.P1_BOX, 	Type.BRICK,	 	Type.EMPTY,		Type.BRICK,	
+			Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.EMPTY, 	Type.EMPTY,	 	Type.EMPTY,		Type.BRICK,
+			Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK, 	Type.BRICK,	 	Type.BRICK,		Type.BRICK,
+		}));
 	}
 	
 	public PuzzleGrid getShadowLevel()
